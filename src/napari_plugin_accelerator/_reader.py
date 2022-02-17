e5f55bcf42f6890fc0b6ae1005bf171df1cc9def
Andy Sweet <andrew.d.sweet@gmail.com>
2022-02-16 16:11:45 -0800

    Remove docstrings

--- src/napari_plugin_accelerator/_reader.py
+++ src/napari_plugin_accelerator/_reader.py
@@ -4,19 +4,6 @@ import zarr
 
 
 def get_reader(path) -> Optional[ReaderFunction]:
-    """Gets the reader associated with a path or list of paths.
-
-    Parameters
-    ----------
-    path : str or list of str
-        Path to file, or list of paths.
-
-    Returns
-    -------
-    Optional[ReaderFunction]
-        If the path is a recognized format, return a function that accepts the
-        same path or list of paths, and returns a list of layer data tuples.
-    """
     if isinstance(path, list):
         # reader plugins may be handed single path, or a list of paths.
         # if it is a list, it is assumed to be an image stack...
@@ -30,18 +17,6 @@ def get_reader(path) -> Optional[ReaderFunction]:
 
 
 def read_zarr(path: str) -> List[LayerData]:
-    """Reads a zarr array as an image layer from a path.
-
-    Parameters
-    ----------
-    path : str
-        Path to zarr directory.
-
-    Returns
-    -------
-    layer_data : List[LayerData]
-        A list containing one napari image layer tuple.
-    """
     z = zarr.open(path, mode='r')
     attributes = {'scale': z.attrs['scale']}
     return [(z, attributes, 'image')]
